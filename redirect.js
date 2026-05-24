d870899059d6a64c6f1c058d51a326f25b454ef2
Mohammad Abu Rayyan <b12524648@ettifagschools.edu.jo>
2026-05-24 22:58:26 +0300

    Delete redirect.js

--- redirect.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  var target = './public/index.html';
-  var query = window.location.search || '';
-  var hash = window.location.hash || '';
-  window.location.replace(target + query + hash);
-})();
